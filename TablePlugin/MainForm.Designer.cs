88d74f072287fe0e826018e92472963c243f0da3
Гелцен Савындай <5842_sgy@keva.int>
2018-05-09 20:49:02 +0700

    New design

--- TablePlugin/MainForm.Designer.cs
+++ TablePlugin/MainForm.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkBoxBuildSeptums = new System.Windows.Forms.CheckBox();
             this.txtBoxSeptumOffset = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.txtBoxSeptumLength = new System.Windows.Forms.TextBox();
@@ -45,22 +46,14 @@
             this.txtBoxTabletopLength = new System.Windows.Forms.TextBox();
             this.buttonBuildTable = new System.Windows.Forms.Button();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
-            this.chkBoxBuildSeptums = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.Controls.Add(this.chkBoxBuildSeptums);
-            this.groupBox1.Controls.Add(this.txtBoxSeptumOffset);
-            this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.txtBoxSeptumLength);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.chkBoxRoundedEdgesTabletop);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.legLengthTextBox);
             this.groupBox1.Controls.Add(this.label3);
@@ -69,16 +62,27 @@
             this.groupBox1.Controls.Add(this.tableTopThicknessTextBox);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.txtBoxTabletopLength);
-            this.groupBox1.Location = new System.Drawing.Point(17, 28);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(581, 500);
+            this.groupBox1.Size = new System.Drawing.Size(463, 173);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Параметры столика";
+            this.groupBox1.Text = "Габариты столика";
+            // 
+            // chkBoxBuildSeptums
+            // 
+            this.chkBoxBuildSeptums.AutoSize = true;
+            this.chkBoxBuildSeptums.Location = new System.Drawing.Point(6, 38);
+            this.chkBoxBuildSeptums.Name = "chkBoxBuildSeptums";
+            this.chkBoxBuildSeptums.Size = new System.Drawing.Size(224, 24);
+            this.chkBoxBuildSeptums.TabIndex = 13;
+            this.chkBoxBuildSeptums.Text = "Построить разделители ";
+            this.chkBoxBuildSeptums.UseVisualStyleBackColor = true;
+            this.chkBoxBuildSeptums.CheckedChanged += new System.EventHandler(this.chkBoxBuildSeptums_CheckedChanged);
             // 
             // txtBoxSeptumOffset
             // 
-            this.txtBoxSeptumOffset.Location = new System.Drawing.Point(441, 384);
+            this.txtBoxSeptumOffset.Location = new System.Drawing.Point(341, 102);
             this.txtBoxSeptumOffset.Name = "txtBoxSeptumOffset";
             this.txtBoxSeptumOffset.Size = new System.Drawing.Size(96, 26);
             this.txtBoxSeptumOffset.TabIndex = 12;
@@ -87,15 +91,15 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(23, 387);
+            this.label6.Location = new System.Drawing.Point(6, 108);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(401, 20);
+            this.label6.Size = new System.Drawing.Size(279, 20);
             this.label6.TabIndex = 11;
-            this.label6.Text = "Отступ разделителей от столешницы, мм (50..100) ";
+            this.label6.Text = "Отступ разделителей, мм (50..100) ";
             // 
             // txtBoxSeptumLength
             // 
-            this.txtBoxSeptumLength.Location = new System.Drawing.Point(441, 331);
+            this.txtBoxSeptumLength.Location = new System.Drawing.Point(341, 70);
             this.txtBoxSeptumLength.Name = "txtBoxSeptumLength";
             this.txtBoxSeptumLength.Size = new System.Drawing.Size(96, 26);
             this.txtBoxSeptumLength.TabIndex = 10;
@@ -104,7 +108,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(23, 337);
+            this.label5.Location = new System.Drawing.Point(6, 76);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(310, 20);
             this.label5.TabIndex = 9;
@@ -113,7 +117,7 @@
             // chkBoxRoundedEdgesTabletop
             // 
             this.chkBoxRoundedEdgesTabletop.AutoSize = true;
-            this.chkBoxRoundedEdgesTabletop.Location = new System.Drawing.Point(27, 433);
+            this.chkBoxRoundedEdgesTabletop.Location = new System.Drawing.Point(12, 342);
             this.chkBoxRoundedEdgesTabletop.Name = "chkBoxRoundedEdgesTabletop";
             this.chkBoxRoundedEdgesTabletop.Size = new System.Drawing.Size(258, 24);
             this.chkBoxRoundedEdgesTabletop.TabIndex = 8;
@@ -123,7 +127,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(23, 230);
+            this.label4.Location = new System.Drawing.Point(6, 134);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(264, 20);
             this.label4.TabIndex = 7;
@@ -131,7 +135,7 @@
             // 
             // legLengthTextBox
             // 
-            this.legLengthTextBox.Location = new System.Drawing.Point(441, 230);
+            this.legLengthTextBox.Location = new System.Drawing.Point(344, 128);
             this.legLengthTextBox.Name = "legLengthTextBox";
             this.legLengthTextBox.Size = new System.Drawing.Size(96, 26);
             this.legLengthTextBox.TabIndex = 6;
@@ -140,7 +144,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(23, 171);
+            this.label3.Location = new System.Drawing.Point(6, 102);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(222, 20);
             this.label3.TabIndex = 5;
@@ -148,7 +152,7 @@
             // 
             // legHeightTextBox
             // 
-            this.legHeightTextBox.Location = new System.Drawing.Point(441, 171);
+            this.legHeightTextBox.Location = new System.Drawing.Point(344, 96);
             this.legHeightTextBox.Name = "legHeightTextBox";
             this.legHeightTextBox.Size = new System.Drawing.Size(96, 26);
             this.legHeightTextBox.TabIndex = 4;
@@ -157,7 +161,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 113);
+            this.label2.Location = new System.Drawing.Point(6, 70);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(274, 20);
             this.label2.TabIndex = 3;
@@ -165,7 +169,7 @@
             // 
             // tableTopThicknessTextBox
             // 
-            this.tableTopThicknessTextBox.Location = new System.Drawing.Point(441, 113);
+            this.tableTopThicknessTextBox.Location = new System.Drawing.Point(344, 64);
             this.tableTopThicknessTextBox.Name = "tableTopThicknessTextBox";
             this.tableTopThicknessTextBox.Size = new System.Drawing.Size(96, 26);
             this.tableTopThicknessTextBox.TabIndex = 2;
@@ -174,7 +178,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 59);
+            this.label1.Location = new System.Drawing.Point(6, 38);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(332, 20);
             this.label1.TabIndex = 1;
@@ -182,7 +186,7 @@
             // 
             // txtBoxTabletopLength
             // 
-            this.txtBoxTabletopLength.Location = new System.Drawing.Point(441, 59);
+            this.txtBoxTabletopLength.Location = new System.Drawing.Point(344, 32);
             this.txtBoxTabletopLength.Name = "txtBoxTabletopLength";
             this.txtBoxTabletopLength.Size = new System.Drawing.Size(96, 26);
             this.txtBoxTabletopLength.TabIndex = 0;
@@ -190,11 +194,9 @@
             // 
             // buttonBuildTable
             // 
-            this.buttonBuildTable.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonBuildTable.Location = new System.Drawing.Point(127, 545);
+            this.buttonBuildTable.Location = new System.Drawing.Point(12, 372);
             this.buttonBuildTable.Name = "buttonBuildTable";
-            this.buttonBuildTable.Size = new System.Drawing.Size(335, 55);
+            this.buttonBuildTable.Size = new System.Drawing.Size(463, 55);
             this.buttonBuildTable.TabIndex = 1;
             this.buttonBuildTable.Text = "Построить столик";
             this.buttonBuildTable.UseVisualStyleBackColor = true;
@@ -203,31 +205,39 @@
             // 
             this.errorProvider1.ContainerControl = this;
             // 
-            // chkBoxBuildSeptums
+            // groupBox2
             // 
-            this.chkBoxBuildSeptums.AutoSize = true;
-            this.chkBoxBuildSeptums.Location = new System.Drawing.Point(27, 286);
-            this.chkBoxBuildSeptums.Name = "chkBoxBuildSeptums";
-            this.chkBoxBuildSeptums.Size = new System.Drawing.Size(224, 24);
-            this.chkBoxBuildSeptums.TabIndex = 13;
-            this.chkBoxBuildSeptums.Text = "Построить разделители ";
-            this.chkBoxBuildSeptums.UseVisualStyleBackColor = true;
-            this.chkBoxBuildSeptums.CheckedChanged += new System.EventHandler(this.chkBoxBuildSeptums_CheckedChanged);
+            this.groupBox2.Controls.Add(this.chkBoxBuildSeptums);
+            this.groupBox2.Controls.Add(this.txtBoxSeptumLength);
+            this.groupBox2.Controls.Add(this.txtBoxSeptumOffset);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(12, 191);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(463, 145);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Параметры разделителей";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(610, 622);
+            this.ClientSize = new System.Drawing.Size(486, 443);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.buttonBuildTable);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chkBoxRoundedEdgesTabletop);
             this.Name = "MainForm";
             this.Text = "Журнальный столик ";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -250,6 +260,7 @@
         private System.Windows.Forms.TextBox txtBoxSeptumLength;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.CheckBox chkBoxBuildSeptums;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
